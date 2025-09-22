6e291968cc9caeeace33d34ffd232f5f12687975
John_Preston <legion.180690@mail.ru>
2025-09-22 22:00:11 +0400

    feat: add autogain per second

--- DataMiningGame/Assets/_scripts/UIManager.cs
+++ DataMiningGame/Assets/_scripts/UIManager.cs
@@ -7,15 +7,20 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _BPSText;
     [SerializeField] private TextMeshProUGUI _BPCText;
 
+    public static UIManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
     private void Start()
     {
         UpdateUI();
     }
-
-    private void Update()
-    {
-        UpdateUI();
-    }
+    
     public void UpdateUI()
     {
         if (GameManager.Instance != null)
